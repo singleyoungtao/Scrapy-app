15345334ee46ad507b27baa897709e2f0a694d93
singleyoungtao <singleyoungtao@gmail.com>
2017-04-27 15:29:21 +0800

    完成从数据库取出url进行爬取

--- nwsuaf/spiders/nwsuaf.py
+++ nwsuaf/spiders/nwsuaf.py
@@ -2,6 +2,11 @@
 #-*-coding:utf-8-*-
 """ nwsuaf spider first test to practise """
 
+import os 
+from pymongo import MongoClient
+# os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'NWSUAF.settings')
+from scrapy import crawler
+from scrapy.crawler import Crawler
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
@@ -12,8 +17,26 @@ from nwsuaf.items import NwsuafItem
 class NusuafSpider(CrawlSpider):
     """ This is spider """
     name = 'nwsuaf'
-    allowed_domains = ['www.nwsuaf.edu.cn']
-    start_urls = ['http://www.nwsuaf.edu.cn']
+    
+    # 暂时无法从settings中取到设置
+    # mongo_uri = crawler.settings['MONGO_URI']
+    # mongo_db = crawler.settings['MONGO_DATABASE']
+
+    client = MongoClient('localhost:27017')
+    db = client.search
+    co = db.url
+    firsturl = co.find_one()
+    allowed_domains = [firsturl['url']]
+    start_first_url = 'http://' + firsturl['url']
+    start_urls = [start_first_url]
+    print(firsturl['url'])
+
+    # allowed_domains = ['www.nwsuaf.edu.cn']
+    # start_urls = ['http://www.nwsuaf.edu.cn']
+    # 此处要注意二者区别
+    # allowed_domains = ['tao-blog.herokuapp.com']
+    # start_urls = ['https://tao-blog.herokuapp.com/']
+
     rules = [
         Rule(LinkExtractor(), callback='parse_response', follow=True),
     ]
